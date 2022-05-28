40b833e0434ed5d11bf9c8c84715213243b2bb44
LukaGrbelja <83420469+LukaGrbelja@users.noreply.github.com>
2022-05-28 19:51:27 +0200

    First commit - Genesis

---
+++ app/src/main/java/com/example/razvoj_android_aplikacija_projekt_treci_razred/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.razvoj_android_aplikacija_projekt_treci_razred;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
